be8137a5601180079339bddd44f253bf28807b3a
sebLopezCot <sclopez2@hotmail.com>
2015-02-01 18:29:47 -0500

    Revert "Experimental Dashboard server"
    This reverts commit 0db28d111d3fd8915d9bd3da799d4f2fa490c166.

--- OpenCV_Robot_Threading/src/org/usfirst/frc/team79/robot/camera/VisionService.java
+++ OpenCV_Robot_Threading/src/org/usfirst/frc/team79/robot/camera/VisionService.java
@@ -1,12 +1,5 @@
 package org.usfirst.frc.team79.robot.camera;
 
-import java.io.DataInputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
@@ -20,8 +13,6 @@ import org.opencv.core.Scalar;
 import org.opencv.highgui.VideoCapture;
 import org.opencv.imgproc.Imgproc;
 
-import com.ni.vision.VisionException;
-
 
 public class VisionService {
 	
@@ -30,8 +21,6 @@ public class VisionService {
 	private static final int FRAME_WIDTH = 320;
 	private static final int FRAME_HEIGHT = 240;
 	private static final double FPS = 7.5;
-	
-	private static final int SERVER_PORT = 1180;
 	
 	private static final double BUFFER_FLUSH_DELAY = 12; // seconds
 	
@@ -43,28 +32,17 @@ public class VisionService {
 	
 	private static Object rawImgMutex = new Object();
 	private static Object measurementMutex = new Object();
-	private static Object outputImgMutex = new Object();
 	
 	private static volatile Mat frame = new Mat();
-	private static volatile Mat output = new Mat();
 	private static volatile AtomicBoolean cameraConnected = new AtomicBoolean(false);
 	private static volatile AtomicBoolean processingImage = new AtomicBoolean(false);
-	private static volatile AtomicBoolean readyToServe = new AtomicBoolean(false);
-
-	private static final int kHardwareCompression = -1;
-	private static final byte[] kMagicNumber = { 0x01, 0x00, 0x00, 0x00 };
-	private List<Byte> m_imageData;
 	
 	private VisionService(){
 		// Start video capture thread
-		m_imageData = new ArrayList<Byte>();
-		
 		Thread videoCaptureThread = new Thread(new VideoCaptureRunnable());
 		Thread imgprocThread = new Thread(new ImageProcessingRunnable());
-		Thread sendToDashboardThread = new Thread(new SendToDashboardRunnable());
 		videoCaptureThread.start();
 		imgprocThread.start();
-		sendToDashboardThread.start();
 	}
 	
 	public static VisionService getInstance(){
@@ -197,35 +175,6 @@ public class VisionService {
 					// If there was a frame, process it
 					if(!frameEmpty){
 						processImage(rawImage, hsv, binImage, outputImage);
-						
-						synchronized (outputImgMutex) {
-							outputImage.copyTo(output); // Copy the output image to the global variable
-						}
-						
-						// Convert mat to JPEG that can be sent to Dashboard
-						byte[] temp = new byte[output.rows() * output.cols() * output.channels()];
-						output.get(0, 0, temp);
-						ByteBuffer buffer = ByteBuffer.wrap(temp);
-						m_imageData.clear();
-						
-						/* Find the start of the JPEG data */
-						int index = 0;
-						while (index < buffer.limit() - 1) {
-							if ((buffer.get(index) & 0xff) == 0xFF
-									&& (buffer.get(index + 1) & 0xff) == 0xD8)
-								break;
-							index++;
-						}
-						while (index < buffer.limit()) {
-							m_imageData.add(buffer.get(index++));
-						}
-
-						if (m_imageData.size() <= 2) {
-							throw new VisionException(
-									"data size of flattened image is less than 2. Try another camera! ");
-						}
-						
-						readyToServe.set(true);
 					}
 				}
 				
@@ -310,98 +259,4 @@ public class VisionService {
 		
 	}
 	
-	private class SendToDashboardRunnable implements Runnable {
-
-		@Override
-		public void run() {
-			try {
-				serve();
-			} catch (IOException e) {
-				// do stuff here
-			} catch (InterruptedException e) {
-				// do stuff here
-			}
-		}
-	}
-
-	public void serve() throws IOException, InterruptedException {
-		ServerSocket socket = new ServerSocket();
-		socket.setReuseAddress(true);
-		InetSocketAddress address = new InetSocketAddress(SERVER_PORT);
-		socket.bind(address);
-
-		while (true) {
-			try {
-				Socket s = socket.accept();
-
-				DataInputStream is = new DataInputStream(s.getInputStream());
-				OutputStream os = s.getOutputStream();
-
-				int fps = is.readInt();
-				int compression = is.readInt();
-				int size = is.readInt();
-
-				if (compression != kHardwareCompression ) {
-					// print to driverstation -->
-					// ("Choose \"USB Camera HW\" on the dashboard");
-					s.close();
-					continue;
-				}
-
-				long period = (long) (1000 / (1.0 * fps));
-				while (true) {
-
-					long t0 = System.currentTimeMillis();
-
-					while (!readyToServe.get())
-						;
-					readyToServe.set(false);
-
-					// write numbers
-					try {
-
-						os.write(kMagicNumber);
-
-						// write size of image
-
-						synchronized (this) {
-							os.write(intTobyteArray(m_imageData.size()));
-
-							byte[] imageData = new byte[m_imageData.size()];
-
-							for (int i = 0; i < m_imageData.size(); i++) {
-								imageData[i] = m_imageData.get(i).byteValue();
-							}
-
-							os.write(imageData);
-						}
-						long dt = System.currentTimeMillis() - t0;
-
-						if (dt < period) {
-							Thread.sleep(period - dt);
-						}
-
-					} catch (IOException ex) {
-						// print error to driverstation
-						break;
-					}
-				}
-
-			} catch (IOException ex) {
-				// print error to driverstation
-				continue;
-			}
-		}
-	}
-	
-	private byte[] intTobyteArray(int n) {
-		byte[] arr = new byte[4];
-
-		arr[0] = (byte) ((n >> 24) & 0xFF);
-		arr[1] = (byte) ((n >> 16) & 0xFF);
-		arr[2] = (byte) ((n >> 8) & 0xFF);
-		arr[3] = (byte) (n & 0xFF);
-
-		return arr;
-	}
 }
